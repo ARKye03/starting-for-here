f183421b1191eab02a1d56141065768c386d23df
ARKye03 <rafa03-dev@proton.me>
2026-01-07 23:09:41 -0300

    feat: add i18n logic

---
+++ src/i18n/utils.ts
@@ -0,0 +1,33 @@
+import { ui, defaultLang } from './ui';
+
+export type Language = keyof typeof ui;
+
+export function getLangFromUrl(url: URL): Language {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as Language;
+  return defaultLang;
+}
+
+export function useTranslations(lang: Language) {
+  return function t(key: keyof typeof ui[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  }
+}
+
+export function getRouteFromUrl(url: URL): string | undefined {
+  const pathname = url.pathname;
+  const parts = pathname?.split('/');
+  const path = parts.pop() || parts.pop();
+
+  if (path === undefined) {
+    return undefined;
+  }
+
+  const currentLang = getLangFromUrl(url);
+
+  if (defaultLang === currentLang) {
+    return path;
+  }
+
+  return path;
+}
